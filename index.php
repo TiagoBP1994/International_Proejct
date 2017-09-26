428973f10abb20bd434b56e6c6f5eb3532993f7b
DanielJelle <DanielJelle@users.noreply.github.com>
2017-09-26 11:38:07 +0200

    added bootstrap + slick to index.php
    added the file footer.inc.php

--- index.php
+++ index.php
@@ -14,9 +14,33 @@ and open the template in the editor.
         $dbh = DbH::getDbH();
         ?>
         <meta charset="UTF-8">
-        <title></title>
+        
+        <!-- BOOTSTRAP - Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- BOOTSTRAP - Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+        <!-- BOOTSTRAP - Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        
+        <!-- Slick Slider CDN -->
+        <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.8.0/slick.css"/>
+        <script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.8.0/slick.min.js"></script>
+        
+        <title>Photo Upload Project | Group 2</title>
     </head>
     <body>
+        
+        <?php 
+        // Includes the header with navigation
+        include './Include/header.inc.php'; 
+        ?>
+        
+        
+        
+        <?php 
+        // Includes the footer
+        include './Include/footer.inc.php'; 
+        ?>
         
     </body>
 </html>
