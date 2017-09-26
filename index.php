df42a2ad9c7b44f7b2c6acf0653e376da8229d42
DanielJelle <DanielJelle@users.noreply.github.com>
2017-09-26 20:02:36 +0200

    changes index.php (deleted slick and added bootstrap thumbnail

--- index.php
+++ index.php
@@ -19,9 +19,6 @@ and open the template in the editor.
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <!-- BOOTSTRAP - Optional theme -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-                
-        <link rel="stylesheet" type="text/css" href="./slick/slick.css"/>
-        <link rel="stylesheet" type="text/css" href="./slick/slick-theme.css"/>
         
         <title>Photo Upload Project | Group 2</title>
     </head>
@@ -32,44 +29,18 @@ and open the template in the editor.
         include './Include/header.inc.php'; 
         ?>
         
-        <?php
-        // Retrieving Images from Database for Display 
-            $sql  = "select color, dimples, noinstock";
-            $sql .= " from golfballs";
-            $sql .= " order by color, dimples";
-            $r = $dbh->query($sql);
-            $a = array();
-            while ($out = $r->fetch()) {
-                $g = new GolfBall();
-                $g->setColor($out['color']);
-                $g->setDimples($out['dimples']);
-                $g->addStock($out['noinstock']);
-                array_push($a, $g);
-            }
-        ?>
-        
-            <div class="slider-for">
-                
-                <?php
-                    //The Loop to print each image to the slider
-                    printf("    <header><h1>%s</h1></header>\n", $title);            // put your code here
-                    print("    <table>\n");
-                    foreach ($a as $gb) {
-                        print($gb);
-                    }
-                    print("    </table>\n");
-                ?>
-                
-                <div>your content</div>
-                <div>your content</div>
-                <div>your content</div>
+        <div class="row">
+            <div class="col-sm-6 col-md-4">
+              <div class="thumbnail">
+                <img src="..." alt="...">
+                <div class="caption">
+                  <h3>Thumbnail label</h3>
+                  <p>...</p>
+                  <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                </div>
+              </div>
             </div>
-        
-            <div class="slider-nav">
-                <div>your content</div>
-                <div>your content</div>
-                <div>your content</div>
-            </div>
+          </div>
         
         <?php 
         // Includes the footer
@@ -80,29 +51,6 @@ and open the template in the editor.
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <!-- BOOTSTRAP - Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        
-        <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-        <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-        <script type="text/javascript" src="./slick/slick.min.js"></script>
-        
-        <script type="text/javascript">
-         $('.slider-for').slick({
-            autoplay: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
-            fade: true,
-            asNavFor: '.slider-nav'
-          });
-          $('.slider-nav').slick({
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            asNavFor: '.slider-for',
-            dots: true,
-            centerMode: true,
-            focusOnSelect: true
-          });
-        </script>
         
     </body>
 </html>
